dd5046921f8770769421bd3a87b4930ac4d72e9c
Kellen Kolbeck & Kendra Ash <student@epicodus.com>
2015-08-05 11:24:23 -0700

    Processes the users entry adn returns it in title case

---
+++ lib/title_case.rb
@@ -0,0 +1,20 @@
+class String
+  define_method(:title_case) do
+    prepositions = ["the", "and", "or", "of", "to", "from","a", "but"]
+    split_title = self.downcase().split()
+    split_title.each_with_index() do |word, index|
+      letters = word.split("")
+      if prepositions.include?(word) && index != 0
+        word.downcase!()
+      elsif letters[0]=="m" && letters[1]=="c"
+        letters[0].capitalize!()
+        letters[2].capitalize!()
+        word = letters.join("")
+        split_title[index]=word
+      else
+        word.capitalize!()
+      end
+    end
+    split_title.join(" ")
+  end
+end
---
+++ spec/title_case_spec.rb
@@ -0,0 +1,28 @@
+require('rspec')
+require('title_case')
+
+describe('String#title_case') do
+  it("it capitalizes the first letter of a word") do
+    expect(("dog").title_case()).to(eq("Dog"))
+  end
+
+  it('it capitalizes the first letter of multiple words') do
+    expect(("dog bird cat").title_case()).to(eq("Dog Bird Cat"))
+  end
+
+   it('it capitalizes all words except prepositions') do
+     expect(("dog and cat").title_case()).to(eq("Dog and Cat"))
+   end
+
+   it('it capitalizes the first letter of the first word of a String') do
+     expect(("from dog to cat").title_case()).to(eq("From Dog to Cat"))
+   end
+
+   it("it does not capitalize the non-letter characters of a String") do
+     expect(("1 dog, 2 cats, and a bird!").title_case()).to(eq("1 Dog, 2 Cats, and a Bird!"))
+   end
+
+   it("it lower-cases capital letters that are not the first letter of a word of a String") do
+     expect(("BIG DOG AND CAT").title_case()).to(eq("Big Dog and Cat"))
+   end
+ end
